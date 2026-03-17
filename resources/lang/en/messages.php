72924366392abe268e3e2e668a1e1e92b2cb9c29
rakanafif <rakandz004@gmail.com>
2026-03-17 01:33:29 +0100

    Update messages.php

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,1 +1,15 @@
 
+return [
+    'welcome' => 'Welcome to Affican Digital',
+    'home' => 'Home',
+    'about' => 'About Us',
+    'services' => 'Services',
+    'library' => 'Digital Library',
+    'community' => 'Community',
+    'login' => 'Login',
+    'register' => 'Register',
+    'be_vender' => 'Vendor Space',
+    'get_quote' => 'Get a Quote',
+    'discover' => 'Discover the platform',
+    'buy_now' => 'Buy Now',
+];
