a7e2005b466f51c0106eebf0c2c3da22cc62883e
rakanafif <rakandz004@gmail.com>
2026-03-17 02:25:45 +0100

    Update terms.blade.php

--- resources/views/policies/terms.blade.php
+++ resources/views/policies/terms.blade.php
@@ -1,1 +1,11 @@
 
+@extends('layouts.app')
+
+@section('content')
+<div class="bg-affican-dark text-white p-10 leading-relaxed">
+    <h1 class="text-electric-blue text-3xl mb-6">Conditions d'utilisation et droits de propriété</h1>
+    <p>1. Tous les livres publiés doivent être la propriété intellectuelle de l'auteur.</p>
+    <p>2. [span_0](start_span)La plateforme prélève une commission de 20% sur chaque vente réussie[span_0](end_span).</p>
+    <p>3. [span_1](start_span)Le contenu du forum doit respecter les normes de la communauté professionnelle[span_1](end_span).</p>
+</div>
+@endsection
