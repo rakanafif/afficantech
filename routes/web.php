0cd1c12f42f09c5e00f3683271178d9524b95830
rakanafif <rakandz004@gmail.com>
2026-03-17 02:17:49 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,7 @@
 
+use App\Http\Controllers\AdminController;
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::post('/approve-book/{id}', [AdminController::class, 'approveBook'])->name('admin.approve.book');
+});
