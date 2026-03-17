32fc795dad064c2dee719b1d20596588e6c3be18
rakanafif <rakandz004@gmail.com>
2026-03-17 03:00:05 +0100

    Update RegisterController.php

--- app/Http/app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/app/Http/Controllers/Auth/RegisterController.php
@@ -4,3 +4,9 @@ use Illuminate\Support\Facades\Mail;
 
 // داخل وظيفة التخزين بعد حفظ بيانات المستخدم:
 Mail::to($user->email)->send(new WelcomeEmail());
+
+use App\Mail\WelcomeEmail;
+use Illuminate\Support\Facades\Mail;
+
+// بعد نجاح عملية إنشاء المستخدم $user
+Mail::to($user->email)->send(new WelcomeEmail());
