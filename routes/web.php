6cbb0864aec71e48a52e56a0227e6868b90029ce
rakanafif <rakandz004@gmail.com>
2026-03-18 00:53:42 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-// هذا السطر سيوجه الزوار فوراً لصفحة الهوم بالألوان الجديدة
+// الصفحة الرئيسية
 Route::get('/', function () {
-    return view('home'); // تأكد أن الملف اسمه home.blade.php داخل مجلد views
+    return view('home');
 });
+
+// صفحة تسجيل الدخول
 Route::get('/login', function () {
     return view('login');
 });
