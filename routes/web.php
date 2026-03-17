7479676c04bc1827841280c44876c6ae713306d6
rakanafif <rakandz004@gmail.com>
2026-03-18 00:52:55 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home'); // تأكد أن الملف اسمه home.blade.php داخل مجلد views
 });
+Route::get('/login', function () {
+    return view('login');
+});
