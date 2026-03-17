3a170813ca9c7b1a2df84e09b3bad65b218b590e
rakanafif <rakandz004@gmail.com>
2026-03-17 02:13:42 +0100

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -1,1 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = ['book_id', 'seller_id', 'total_amount', 'site_commission', 'seller_net', 'payment_status'];
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function seller() {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+}
 
