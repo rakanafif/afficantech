57ec117a2bac52ae7e34c3c9475aefe38b8108a9
rakanafif <rakandz004@gmail.com>
2026-03-17 02:25:08 +0100

    Update logo.blade.php

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,1 +1,4 @@
 
+<div class="fixed bottom-4 right-4 z-50">
+    <img src="{{ asset('images/affican-digital-logo.png') }}" alt="Affican Digital" class="h-12 w-auto opacity-80 hover:opacity-100 transition-opacity">
+</div>
