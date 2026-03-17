2cbdbde13ceee362a4bbbe610c0b841ac743d452
rakanafif <rakandz004@gmail.com>
2026-03-17 06:33:29 +0100

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,4 +1,18 @@
 
-protected $except = [
-    '/whatsapp/webhook', // استثناء مسار الواتساب من حماية CSRF
-];
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        'whatsapp/webhook',
+    ];
+}
