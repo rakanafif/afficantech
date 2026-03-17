3fcd7732e295ecf42c48d0794db61b66b279ec39
rakanafif <rakandz004@gmail.com>
2026-03-17 22:50:20 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
-// أمر مسح الذاكرة وبناء قاعدة البيانات
+
 Route::get('/run-migrate', function() {
     try {
-        // السطر السحري لمسح الذاكرة القديمة
-        \Illuminate\Support\Facades\Artisan::call('optimize:clear'); 
-        
-        // بناء الجداول
+        \Illuminate\Support\Facades\Artisan::call('optimize:clear');
         \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-        
         return "تم مسح الذاكرة القديمة وبناء الجداول بنجاح! قاعدة البيانات جاهزة 100% 🎉";
     } catch (\Exception $e) {
         return "حدث خطأ: " . $e->getMessage();
