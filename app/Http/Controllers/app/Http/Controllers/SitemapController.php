1f8afa11dd70b398805ffa7a1da67369feac5a97
rakanafif <rakandz004@gmail.com>
2026-03-17 03:03:27 +0100

    Update SitemapController.php

--- app/Http/Controllers/app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/app/Http/Controllers/SitemapController.php
@@ -1,1 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\Post;
+use Illuminate\Http\Response;
+
+class SitemapController extends Controller
+{
+    public function index()
+    {
+        $books = Book::where('status', 'published')->get();
+        $posts = Post::where('is_approved', true)->get();
+
+        return response()->view('sitemap', [
+            'books' => $books,
+            'posts' => $posts,
+        ])->header('Content-Type', 'text/xml');
+    }
+}
 
