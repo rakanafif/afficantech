72e8491e961db59b8a7bb933f7e5f4b737637c15
rakanafif <rakandz004@gmail.com>
2026-03-17 02:50:44 +0100

    Update WelcomeEmail.php

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -1,1 +1,19 @@
 
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function build()
+    {
+        return $this->subject('Bienvenue chez Affican Digital')
+                    ->view('emails.welcome'); // يشير إلى نص الرسالة الذي جهزناه
+    }
+}
