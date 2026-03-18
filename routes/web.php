7c59149a10d79f34cd0301468d2f9ffaa52db0fb
rakanafif <rakandz004@gmail.com>
2026-03-18 01:10:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,27 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 // الصفحة الرئيسية
 Route::get('/', function () {
     return view('home');
 });
 
-// صفحة تسجيل الدخول
+// صفحة الدخول
 Route::get('/login', function () {
     return view('login');
 });
+
+// صفحة التسجيل
 Route::get('/register', function () {
     return view('register');
+});
+
+// محرك تغيير اللغة
+Route::get('/lang/{locale}', function ($locale) {
+    if (in_array($locale, ['ar', 'fr', 'en'])) {
+        Session::put('locale', $locale);
+    }
+    return redirect()->back();
 });
