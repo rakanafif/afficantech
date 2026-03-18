cb0e6ded7a429ccb2e8b68b074da2de92cd501e9
rakanafif <rakandz004@gmail.com>
2026-03-18 01:16:39 +0100

    Update messages.php

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -1,15 +1,6 @@
-
+<?php
 return [
-    'welcome' => 'مرحباً بكم في Affican Digital',
-    'home' => 'الصفحة الرئيسية',
-    'about' => 'من نحن',
-    'services' => 'خدماتنا',
-    'library' => 'المكتبة الرقمية',
-    'community' => 'المجتمع',
-    'login' => 'تسجيل الدخول',
-    'register' => 'إنشاء حساب',
-    'be_vender' => 'لوحة البائعين',
-    'get_quote' => 'اطلب عرضاً',
-    'discover' => 'اكتشف المنصة',
-    'buy_now' => 'شراء الآن',
+    'welcome_title' => 'حكمة العقل وشغف القلب',
+    'welcome_subtitle' => 'مرحباً بكم في Affican Digital. وجهتكم الأولى لاكتشاف ونشر الروائع الرقمية.',
+    'btn_explore' => 'استكشف المكتبة',
 ];
