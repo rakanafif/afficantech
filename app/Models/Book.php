c956c0f67ab2c44d190f256a7f06cafe2ae0a90c
rakanafif <rakandz004@gmail.com>
2026-03-19 05:48:48 +0100

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -1,1 +1,13 @@
 
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    // السماح بحفظ هذه البيانات في قاعدة البيانات
+    protected $fillable = ['title', 'description', 'price', 'cover_path', 'user_id'];
+}
+
