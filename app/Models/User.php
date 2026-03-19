961732278bb9dadab0b4ce4cf327769ea948e46c
rakanafif <rakandz004@gmail.com>
2026-03-19 02:01:40 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,1 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    // هذه المصفوفة تسمح بحفظ البيانات في القاعدة
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    // حماية كلمة المرور
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
 
